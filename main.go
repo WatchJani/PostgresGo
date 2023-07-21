2d752a1160899a7abb473c2f0aaa4a7d31fa1ec8
WatchJani <jankokondic84@gmail.com>
2023-07-21 15:03:11 +0200

    db structure init

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/WatchJani/PostgresGo/postgres"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("ASD")
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	config := postgres.Config{
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		User:     os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASSWORD"),
+		Name:     os.Getenv("DB_NAME"),
+	}
+
+	fmt.Println(config)
 }
--- postgres/postgres.go
+++ postgres/postgres.go
@@ -2,7 +2,7 @@ package postgres
 
 type Config struct {
 	Host     string
-	Port     int
+	Port     string
 	User     string
 	Password string
 	Name     string
